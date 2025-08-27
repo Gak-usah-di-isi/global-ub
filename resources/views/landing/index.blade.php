1842e51bb0bf0121bf9ddbb6bb4c80d707f83038
Anas Khalif Muttaqien <anaskhalif995@gmail.com>
2025-08-27 15:09:19 +0700

    feat : layouting landing page

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing Page</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1 class="text-xl text-red-500">Welcome to Our Landing Page</h1>
+    <p class="text-gray-700">This is a simple landing page template.</p>
+</body>
+
+</html>
