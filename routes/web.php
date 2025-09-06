1d15614864a3d67a9be8bbc555227bf1dc70a8e8
Anas Khalif Muttaqien <anaskhalif995@gmail.com>
2025-09-06 10:55:36 +0700

    feat add branch develop head branch

--- routes/web.php
+++ routes/web.php
@@ -3,46 +3,42 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('landing.index');
+});
 
-// Route::get('/', function () {
-//     return view('landing.index');
-// });
+Route::get('/about', function () {
+    return view('landing.about');
+});
 
-// Route::get('/about', function () {
-//     return view('landing.about');
-// });
+Route::get('/news', function () {
+    return view('landing.news');
+});
 
-// Route::get('/news', function () {
-//     return view('landing.news');
-// });
+Route::get('/event', function () {
+    return view('landing.event');
+});
 
-// Route::get('/event', function () {
-//     return view('landing.event');
-// });
-
-// Route::get('/study', function () {
-//     return view('landing.study');
-// });
+Route::get('/study', function () {
+    return view('landing.study');
+});
 
 
-// Route::get('/story', function () {
-//     return view('landing.stories');
-// });
+Route::get('/story', function () {
+    return view('landing.stories');
+});
 
-// Route::get('/testimonial', function () {
-//     return view('landing.testimonials');
-// });
+Route::get('/testimonial', function () {
+    return view('landing.testimonials');
+});
 
-// Route::get('/detail-study', function () {
-//     return view('landing.detail-study');
-// });
+Route::get('/detail-study', function () {
+    return view('landing.detail-study');
+});
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
