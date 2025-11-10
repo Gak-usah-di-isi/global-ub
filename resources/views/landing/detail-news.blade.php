bc062f09bdae72cad45e980ed50d935072d4ee05
Anas Khalif Muttaqien <anaskhalif995@gmail.com>
2025-11-10 14:33:08 +0700

    fix : description gallery

--- resources/views/landing/detail-news.blade.php
+++ resources/views/landing/detail-news.blade.php
@@ -39,8 +39,8 @@
 
     {{-- News Content Section --}}
     <section class="lg:max-w-6xl mx-auto px-4 sm:px-6 py-12 lg:py-20 lg:px-8">
-        <div class="w-full mx-auto mb-8">
-            <img src="{{ asset('storage/' . $news->image) }}" class="h-full rounded-xl w-full bg-cover bg-center"
+        <div class="w-full mx-auto mb-8 max-h-[500px] overflow-hidden flex items-center justify-center">
+            <img src="{{ asset('storage/' . $news->image) }}" class="w-full h-auto max-h-[500px] rounded-xl object-cover"
                 alt="{{ $news->title }}">
         </div>
         <div class="flex w-full text-xs mt-12 mb-4 text-neutral-400">
