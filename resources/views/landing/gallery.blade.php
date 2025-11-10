fb894175cc764f6f706b6f8a858abb5efc6a2f20
Anas Khalif Muttaqien <anaskhalif995@gmail.com>
2025-11-10 13:48:03 +0700

    fix : description gallery

--- resources/views/landing/gallery.blade.php
+++ resources/views/landing/gallery.blade.php
@@ -58,7 +58,7 @@
                         </h2>
                         <p
                             class="font-inter font-light text-[13px] md:text-[16px] lg:text-[17px] leading-[18px] md:leading-[24px] lg:leading-[29.25px] text-[#F9FAFB] pr-12 break-words">
-                            {{ $gallery->description }}
+                            {{ Str::limit($gallery->description, 100, '...') }}
                         </p>
                     </div>
                 </div>
