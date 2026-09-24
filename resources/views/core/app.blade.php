c3385526853530f0214aac188bb3b25bce311eac
Pstar7s <faizhenrik@student.ub.ac.id>
2026-09-24 21:20:52 +0700

    feat : integrate Google Analytics tracking

--- resources/views/core/app.blade.php
+++ resources/views/core/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{ asset('/images/ub.png') }}">
     <title>@yield('title')</title>
+    @include('core.partials.analytics')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @include('core.partials.link')
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
---
+++ resources/views/core/partials/analytics.blade.php
@@ -0,0 +1,13 @@
+@if (config('analytics.measurement_id'))
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('analytics.measurement_id') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', '{{ config('analytics.measurement_id') }}');
+    </script>
+@endif
